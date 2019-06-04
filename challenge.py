3898699a7ca2a8ab61c59597d61db829e586f0dc
Manfred Chan <mchan21@binghamton.edu>
2019-06-04 04:10:20 -0700

    Initial commit

---
+++ challenge.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#cantina challenge
+
+def getDocument():
+    pass
+
+def parseDocument():
+    pass
+
+def findLabel():
+    pass
+    
+def harness():
+    while True:
+        query = input("Enter query, or \"EXIT\" to quit\n> ")
+        if query == "EXIT":
+            break
+        
+if __name__ == "__main__":
+    harness()
+    
+    
