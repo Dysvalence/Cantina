838e8c4a43f7a21647556e933771380c5b6fa55e
Manfred Chan <mchan21@binghamton.edu>
2019-06-10 05:51:16 -0700

    Cleanup

--- challenge.py
+++ challenge.py
@@ -6,58 +6,50 @@
 import json
 import requests
 import pprint
-#subview contentview input control can contaain further stuff
-# class =/= className, identifier
-#
+
 
 CAN_CONTAIN = ["subviews", "contentView", "input", "control"]
 
 DOCUMENT_URL = "https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json"
 
+IDENTIFIER = "identifier"
+CLASSNAMES = "classNames"
+CLASS = "class"
+
 def getDocument(url):
     return requests.get(url).json()
 
 def search(query, doc):
-    #doc will either be a dict, or a list
-    #wait nvm, the nests would be under CAN_CONTAIN
+
     def helper(query, doc, querytype):
         retval = []
-        #print("POOTIS")
-        #print(type(doc))
+        
         if isinstance(doc, list):
-            #print("list found, len "+str(len(doc)))
             for x in doc:
                 if isinstance(x, dict):
                     retval.extend(helper(query, x, querytype))
-                else:
-                    print ("anomaly"+str(type(x)))
-            
-
+                
         if isinstance(doc, dict):
-            #print("dict found")
-            #print(doc.keys())
-
-            #if querytype in doc and doc[querytype]==query:
             if querytype in doc:
-                if doc[querytype]==query:
+                if isinstance(doc[querytype], list):
+                    for x in doc[querytype]:
+                        if x==query:
+                            retval.append(doc)
+                elif doc[querytype]==query:
                     retval.append(doc)
-                    #print("KEY FOUND")
-                else:
-                    pass
-                    #print (doc[querytype])
+
             for x in CAN_CONTAIN:
-                if x in doc and doc[x] is not None and len(doc[x])!=0: # or doc[x] type is idk whatever
+                if x in doc and doc[x] is not None and len(doc[x])!=0:
                     retval.extend(helper(query, doc[x], querytype))
-                    #helper(query, doc[x], querytype, retval)
             
         return retval 
         
     if query[0] == "#":
-        return helper(query, doc, "identifier")
+        return helper(query[1:], doc, IDENTIFIER)
     elif query[0] == ".": #classname
-        return helper(query, doc, "className")
+        return helper(query[1:], doc, CLASSNAMES) 
     else: #class
-        return helper(query, doc, "class")
+        return helper(query, doc, CLASS)
 
 
 def prettyprint(views):
@@ -72,14 +64,13 @@ def harness(doc):
         if query == "EXIT":
             break    
         views = search(query, doc)
-        prettyprint(views)
-
-        
-        
-        
+        if len(views)==0:
+            print("KEY NOT FOUND")
+        else:
+            prettyprint(views)
+  
         
 if __name__ == "__main__":
-    #pprint.pprint(getDocument(DOCUMENT_URL))
     harness(getDocument(DOCUMENT_URL))
     
     
